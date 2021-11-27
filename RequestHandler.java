e061b11aa52925c07776f5a81020ae2238e328b2
denniskolomiyets <92087842+denniskolomiyets@users.noreply.github.com>
2021-11-27 15:48:32 +0000

    Update RequestHandler.java

--- RequestHandler.java
+++ RequestHandler.java
@@ -1,8 +1,29 @@
+import java.io.IOException;
+
 public class RequestHandler {
     public static void newReservation(String name, String type, String checkIn, String checkOut, int numberOfRooms){
         Reservation reservation = new Reservation(name, type, checkIn, checkOut, numberOfRooms);
         reservation.getRoomTypeList();
         reservation.getTotalCost();
         reservation.addOccupancy();
+        String rooms = "";
+        rooms = reservation.getRoomType(0);
+        for(int i = 1; i < reservation.getNumberOfRooms(); i++){
+            rooms = rooms + "|" + reservation.getRoomType(i);
+        }
+        try {
+        FileWriter newWrite = new FileWriter(reservation.getReservationNum(), name,  type, checkIn, checkOut, numberOfRooms, rooms);
+        FileWriter writePayments = new FileWriter("Payed", reservation.getTotalCost());
+        } catch (IOException e) {
+            e.getStackTrace();
+        }
+    }
+
+    public static void displayReservations(){
+        String reservationList = FileReader.readReservations();
+        String[] tokens = reservationList.split("%");
+        for(int i = 0; i < FileReader.getListSize(); i++){
+            System.out.println(tokens[i]);
+        }
     }
 }
