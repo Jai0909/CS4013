02bc8251e39b37473874406c15e28c532f63a6ff
Clevercael1 <91101191+Clevercael1@users.noreply.github.com>
2021-11-27 22:57:17 +0000

    FileWriter
    Added a method called removeReservation that removes a specified reservation from the reservations.csv file.
    Updated the Java Documentation.

--- FileWriter.java
+++ FileWriter.java
@@ -1,17 +1,17 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * @author Cael O'Flaherty
- *
- * This class writes data to both payment.csv and reservations.csv.
+ * This class writes data to payment.csv and reservations.csv. It also removes a reservation from a csv file.
  */
 public class FileWriter {
-    File reservationWriterFile = new File("src/reservations.csv");
-    File paymentWriter = new File("src/payment.csv");
-
+    //Reservation csv writer data fields.
+    private File reservationWriterFile = new File("src/reservations.csv");
     private int reservationNum;
     private String reservationName;
     private String reservationType;
@@ -20,41 +20,39 @@ public class FileWriter {
     private int numberOfRooms;
     private String roomType;
 
-
+    //Payment csv writer data fields.
+    private File paymentWriter = new File("src/payment.csv");
     private String isItPaid;
     private double totalCost;
 
     /**
-     * This constructor makes a FileWriter object that writes data to reservations.csv
+     * This constructor makes a FileWriter object that writes data to reservations.csv.
      *
      * @param reservationNum
-     * @param name
-     * @param type
+     * @param reservationName
+     * @param reservationType
      * @param checkIn
      * @param checkOut
      * @param numberOfRooms
      * @param roomType
      * @throws IOException
      */
-    public FileWriter(int reservationNum, String name, String type, String checkIn, String checkOut, int numberOfRooms, String roomType) throws IOException {
+    public FileWriter(int reservationNum, String reservationName, String reservationType, String checkIn, String checkOut, int numberOfRooms,
+                      String roomType) throws IOException {
         this.reservationNum = reservationNum;
-        reservationName = name;
-        reservationType = type;
+        this.reservationName = reservationName;
+        this.reservationType = reservationType;
         this.checkIn = checkIn;
         this.checkOut = checkOut;
         this.numberOfRooms = numberOfRooms;
         this.roomType = roomType;
 
-        //converts the arrays to strings
-
-
-
-        //creates a new csv if it doesn't already exist
+        //creates a new csv file if one doesn't already exist.
         if (!reservationWriterFile.exists()) {
             reservationWriterFile.createNewFile();
         }
 
-        //Prints the data to the csv file
+        //Prints the data to the reservations.csv file.
         PrintWriter reservationWriter = new PrintWriter(new java.io.FileWriter(reservationWriterFile, true));
         reservationWriter.append(reservationNum + "," + reservationName + "," + reservationType + "," + checkIn + "," + checkOut + "," +
                 numberOfRooms + "," + roomType + "\n");
@@ -62,7 +60,8 @@ public class FileWriter {
     }
 
     /**
-     * This constructor makes a FileWriter object that writes data to payment.csv
+     * This constructor makes a FileWriter object that writes data to payment.csv.
+     *
      * @param isItPaid
      * @param totalCost
      * @throws IOException
@@ -71,19 +70,80 @@ public class FileWriter {
         this.isItPaid = isItPaid;
         this.totalCost = totalCost;
 
-        //creates a new csv if it doesn't already exist
+        //creates a new csv if it doesn't already exist.
         if (!paymentWriter.exists()) {
             paymentWriter.createNewFile();
         }
 
-        //Prints the data to the csv file
+        //Prints the data to the csv file.
         PrintWriter costWriter = new PrintWriter(new java.io.FileWriter(paymentWriter, true));
         costWriter.append(isItPaid + "," + totalCost + "\n");
         costWriter.close();
     }
 
-    public static void removeReservation(int number){
-        System.out.println("Removes item number: " + number);
-        //removes the line with the number matching the reservation number in reservation.csv
+    /**
+     * this method removes a specified reservation from the reservations.csv file.
+     *
+     * @param number
+     */
+    public static void removeReservation(int number) {
+        File file = new File("src/reservations.csv");
+        int reservationInt;
+        String reservationName;
+        String reservationType;
+        String checkIn;
+        String checkOut;
+        String numberOfRooms;
+        String roomType;
+        String line;
+        ArrayList<TempRes> tempStore = new ArrayList<>();
+
+        try {
+            //read in the data from the reservations file, creates a TempRes object and stores data in an arraylist of type TempRes.
+            Scanner input = new Scanner(file);
+            while (input.hasNextLine()) {
+                line = input.nextLine();
+                String[] tokens = line.split(",");
+                String reservationString = tokens[0];
+                reservationInt = Integer.parseInt(reservationString);
+                reservationName = tokens[1];
+                reservationType = tokens[2];
+                checkIn = tokens[3];
+                checkOut = tokens[4];
+                numberOfRooms = tokens[5];
+                roomType = tokens[6];
+                TempRes store = new TempRes(reservationInt, reservationName, reservationType, checkIn, checkOut, numberOfRooms, roomType);
+                tempStore.add(store);
+            }
+
+            //Iterates the array list. if the reservation number is equal to the reservation you want to remove, remove it from the arraylist.
+            for (int i = 0; i < tempStore.size(); i++) {
+                if (tempStore.get(i).getReservationNum() == number) {
+                    tempStore.remove(i);
+                    System.out.println("Removed item number: " + number);
+                }
+            }
+
+            //Formats the arraylist and stores it in a string.
+            String tempStoreString = "";
+            for (int i = 0; i < tempStore.size(); i++) {
+                tempStoreString += (tempStore.get(i).toString());
+            }
+
+            //print the stored reservations from the arraylist back to the csv file. This overwrites the original reservations.csv file.
+            PrintWriter reservationWriter = new PrintWriter(new java.io.FileWriter(file, false));
+            reservationWriter.write(tempStoreString);
+            reservationWriter.close();
+
+            //catch for scanner.
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("reservations.csv not found");
+
+            //Catch for FileWriter.
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Unable to write to reservations.csv. Make sure file is in place and available");
+        }
     }
 }
