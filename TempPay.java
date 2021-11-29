f0d7737da22a83a829e857f6e960e44a8f3b8186
Clevercael1 <91101191+Clevercael1@users.noreply.github.com>
2021-11-29 00:24:48 +0000

    Created TempPay
    Created TempPay object
    added Getters for several data fields
    added toString method

---
+++ TempPay.java
@@ -0,0 +1,83 @@
+/**
+ * @author Cael O'Flaherty
+ * This Class creates an object for the payment details to be temporarily stored in.
+ */
+public class TempPay {
+    int resNum;
+    String checkInDate;
+    String checkOutDate;
+    String roomTypeForHotel;
+    double totalCost;
+
+    /**
+     * Constructor for TempPay object.
+     *
+     * @param resNum
+     * @param checkInDate
+     * @param checkOutDate
+     * @param roomTypeForHotel
+     * @param totalCost
+     */
+    public TempPay(int resNum, String checkInDate, String checkOutDate, String roomTypeForHotel, double totalCost) {
+        this.resNum = resNum;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.roomTypeForHotel = roomTypeForHotel;
+        this.totalCost = totalCost;
+    }
+
+    /**
+     * Getter for Reservation number.
+     *
+     * @return int Reservation Number.
+     */
+    public int getReservationNum() {
+
+        return resNum;
+    }
+
+    /**
+     * Getter for the check in date.
+     *
+     * @return String check in date.
+     */
+    public String getCheckInDate() {
+        return checkInDate;
+    }
+
+    /**
+     * Getter for the check out date.
+     *
+     * @return String check out date.
+     */
+    public String getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    /**
+     * Getter for the room type. Used to determine what hotel is selected.
+     *
+     * @return String room type.
+     */
+    public String getRoomTypeForHotel() {
+        return roomTypeForHotel;
+    }
+
+    /**
+     * Getter for the total cost of a payment.
+     *
+     * @return double total cost.
+     */
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    /**
+     * ToString method for the TempPay object.
+     *
+     * @return string displaying all data in a TempPay object.
+     */
+    public String toString() {
+        return (resNum + "," + checkInDate + "," + checkOutDate + "," + roomTypeForHotel + "," + totalCost + "\n");
+    }
+}
