b582c205f3cc5bcb4e69b8120efafc1dc8b39e16
denniskolomiyets <92087842+denniskolomiyets@users.noreply.github.com>
2021-11-30 17:20:54 +0000

    added more comments

--- DateChecker.java
+++ DateChecker.java
@@ -20,13 +20,14 @@ public class DateChecker {
         try {
             Date d1 = dateFormat.parse(start_date);
             Date d2 = dateFormat.parse(end_date);
-
+            //finds difference between 2 dates
             long differenceInDays = TimeUnit.MILLISECONDS.toDays(d2.getTime() - d1.getTime());
 
             if(differenceInDays == 0){
                 System.out.println("Same day");
                 return 0;
             } else {
+                //adds one to include the last day that is reserved also
                 return (int)differenceInDays+1;
             }
 
@@ -37,11 +38,12 @@ public class DateChecker {
     }
 
     /**
-    * This methos checks if an year is a leap year or not
+    * This method checks if an year is a leap year or not
     * @param year
     * @return returns true if year is leap year
     */
     public static boolean isLeap(int year){
+      //returns true if year inputted is a leap year
       return (((year%4==0)&&(year%100!=0))||(year%400==0));
     }
     /**
@@ -66,13 +68,16 @@ public class DateChecker {
           }
         }
         else if(month1==2){
+            //checks if month is February and if their days exceed 28 if its a leap year for the first date
           if((!isLeap(year1))&&day1>28){
             return false;
           }
+            //checks if month is February and if their days exceed 29 if its not a leap year for the first date
           else if((isLeap(year1))&&day1>29){
             return false;
           }
         }
+          //checks if months with 31 days, day value exceeds 31 for the first date
         else if(month1==1||month1==3||month1==5||month1==7||month1==8||month1==10||month1==12){
           if(day1>31){
             return false;
@@ -81,19 +86,23 @@ public class DateChecker {
         else{
           return false;
         }
+          //checks if months with 30 days, day value exceeds 30 for the first date
         if(month2==4||month2==6||month2==9||month2==11){
           if(day2>30){
             return false;
           }
         }
+           //checks if month is February and if their days exceed 28 if its a leap year for the second date
         else if(month2==2){
           if((!isLeap(year2))&&day2>28){
             return false;
           }
+            //checks if month is February and if their days exceed 29 if its not a leap year for the second date
           else if((isLeap(year2))&&day2>29){
             return false;
           }
         }
+           //checks if months with 31 days, day value exceeds 31 for the second date
         else if(month2==1||month2==3||month2==5||month2==7||month2==8||month2==10||month2==12){
           if(day2>31){
             return false;
@@ -121,6 +130,7 @@ public class DateChecker {
         try {
             Date realDate = dateFormat.parse(date);
             cal.setTime(realDate);
+            //adds 3 to be usable from hotels.csv
             return cal.get(Calendar.DAY_OF_WEEK)+3;
         } catch (ParseException e){
             e.printStackTrace();
@@ -135,6 +145,7 @@ public class DateChecker {
     public static String getLocalDate() {
         String date = LocalDate.now().toString();
         String[] tokens = date.split("-");
+        //returns todays date in our usable format
         return tokens[2] + "-" + tokens[1] + "-" + tokens[0];
     }
 
@@ -152,27 +163,31 @@ public class DateChecker {
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
         Calendar calender = Calendar.getInstance();
 
-
+        //creates HashMap that loads information into memory
         Map<String, List<TempRes>> reservations = TempRes.loadCSV("CsvFiles/reservations.csv");
         if (reservations == null) {
             return true;
         }
-
+        //r stores all reservations with our desired roomtype
         List<TempRes> r = reservations.get(roomType);
+        //checks if r is empty automatically true
         if (r == null) {
             return true;
         }
 
         try {
+            //gets number of days the reservation should take place over
             int daysRequired = findDifference(checkin, checkout);
+            //creates list to store how many rooms are occupied for a particular day
             ExistingReservation[] occupiedRooms = new ExistingReservation[daysRequired];
+            //creates check in date variable
             Date in = dateFormat.parse(checkin);
             calender.setTime(in);
             for(int i = 0; i < daysRequired; i++){
                 occupiedRooms[i] = new ExistingReservation(calender.getTime(), 0);
                 calender.add(Calendar.DATE, 1);
             }
-
+            //checks each day and each reservation and if there are our type of room reserved for that day. If so it increments that days occupied rooms by 1
             for (ExistingReservation er : occupiedRooms) {
                 for (TempRes res : r) {
                     Date checkIn = dateFormat.parse(res.checkIn);
@@ -183,6 +198,7 @@ public class DateChecker {
                 }
             }
 
+            //checks each day for if any of the reserved rooms for that day exceeds the maximum allowed
             for(ExistingReservation er : occupiedRooms) {
                 if(er.getReservedTotal() >= FileReader.roomTypeNumberOfRooms(roomType)){
                     return false;
